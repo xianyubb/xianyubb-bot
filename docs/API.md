dd4fa9aaffccc05dd2f12c894e58e7b97e48c649
xianyubb <2149656630@qq.com>
2023-08-06 21:00:20 +0800

    添加了更多的api，更新了文档

--- docs/API.md
+++ docs/API.md
@@ -120,3 +120,131 @@
 > `bot.get_group_msg_history(message_seq,group_id)`
 
 不提供起始序号将默认获取最新的消息
+
+### 图片
+
+#### 获取图片信息
+
+> `bot.get_image(file)`
+
+#### 检查是否可以发送图片
+
+> `bot.can_send_image()`
+
+#### 图片 OCR
+
+> `bot.ocr_image(image)`
+
+目前图片OCR接口仅支持接受的图片
+
+### 语音
+
+#### 获取语音
+
+> `bot.get_record(file,out_format)`
+
+要使用此接口, 通常需要安装 ffmpeg, 请参考 OneBot 实现的相关说明。
+
+#### 检查是否可以发送语音
+
+> `bot.can_send_record()`
+
+该 API 无需参数
+
+### 处理
+
+#### 处理加好友请求
+
+> `bot.set_friend_add_request(flag,approve,remark)`
+
+该 API 无响应数据
+
+#### 处理加群请求/邀请
+
+> `bot.set_group_add_request(flag,sub_type,approve,reason)`
+
+该 API 无响应数据
+
+### 群信息
+
+#### 获取群信息
+
+> `bot.get_group_info(group_id,no_cache)`
+
+如果机器人尚未加入群, group_create_time, group_level, max_member_count 和 member_count 将会为0
+
+#### 获取群列表
+
+> `bot.get_group_list(no_cache)`
+
+响应内容为 json 数组, 每个元素和上面的 get_group_info 接口相同。
+
+#获取
+
+#### 获取群成员信息
+
+> `botget_group_member_info(group_id,user_id,no_cache)`
+
+#### 获取群成员列表
+
+> `botget_group_member_list(group_id,no_cache)`
+
+#### 获取群荣誉信息
+
+> `bot.get_group_honor_info(gruop_id,type)`
+
+#### 获取群系统消息
+
+> `bot.get_group_system_msg()`
+
+如果列表不存在任何消息, 将返回 `null`
+
+#### 获取精华消息列表
+
+> `bot.get_essence_msg_list(group_id)`
+
+#### 获取群 @全体成员 剩余次数
+
+> `bot.get_group_at_all_remain(group_id)`
+
+### 群设置
+
+#### 设置群名
+
+> `bot.set_group_name(group_id,group_name)`
+
+该 API 无响应数据
+
+#### 设置群头像
+
+> `bot.set_group_portrait(group_id,file,cache)`
+
+[1] file 参数支持以下几种格式：
+
+* 绝对路径, 例如 file:///C:\\Users\Richard\Pictures\1.png, 格式使用 file URI
+* 网络 URL, 例如 http://i1.piimg.com/567571/fdd6e7b6d93f1ef0.jpg
+* Base64 编码, 例如 base64://iVBORw0KGgoAAAANSUhEUgAAABQAAAAVCAIAAADJt1n/AAAAKElEQVQ4EWPk5+RmIBcwkasRpG9UM4mhNxpgowFGMARGEwnBIEJVAAAdBgBNAZf+QAAAAABJRU5ErkJggg==
+
+[2] cache参数: 通过网络 URL 发送时有效, 1表示使用缓存, 0关闭关闭缓存, 默认 为1
+
+[3] 目前这个API在登录一段时间后因cookie失效而失效, 请考虑后使用
+
+#设置群
+
+#### 设置群管理员
+
+> `bot.set_group_admin(group_id,user_id,enable)`
+
+该 API 无响应数据
+
+#### 设置群名片 ( 群备注 )
+
+> `bot.set_group_card(group_id,user_id,card)`
+
+该 API 无响应数据
+
+#### 设置群组专属头衔
+
+> `bot.set_group_special_title(group_id,user_ifd,special_title,duration)`
+
+该 API 无响应数据
--- xianyubb-bot/src/api.ts
+++ xianyubb-bot/src/api.ts
@@ -1,6 +1,6 @@
 import { WebSocket } from "ws";
 import * as events from "./Events"
-import data, { MessageType, Sender } from "./data";
+import data, { MessageType, Returnecho } from "./data";
 import { EventEmitter } from 'events';
 import * as uuid from "uuid";
 
@@ -11,6 +11,7 @@ export class Bot {
   public WebSocketEvents!: events.WebSocketEvent;
   public echoEvent!: EventEmitter
   bot: WebSocket;
+
   constructor(ws: string) {
     this.bot = new WebSocket(ws)
     this.BotEvents = events.bot
@@ -23,10 +24,10 @@ export class Bot {
       this.Afterconnect(JSON.parse(events.data.toString()))
     }
   }
+
   private connect() {
     this.WebSocketEvents.emit("connect")
   }
-
 
   private Afterconnect(msg: any) {
     this.WebSocketEvents.emit("message", msg)
@@ -82,6 +83,7 @@ export class Bot {
       }))
     })
   }
+
   /**
    * 获取企点账号信息 (该API只有企点协议可用)
    */
@@ -99,6 +101,7 @@ export class Bot {
       }))
     })
   }
+
   /**
    * 获取在线机型 (有关例子可从 https://github.com/Mrs4s/go-cqhttp/pull/872#issuecomment-831180149 找到)
    * @param model
@@ -120,6 +123,7 @@ export class Bot {
       }))
     })
   }
+
   /**
    * 设置在线机型 (有关例子可从 https://github.com/Mrs4s/go-cqhttp/pull/872#issuecomment-831180149 找到)
    * 该 API 没有响应数据
@@ -144,6 +148,7 @@ export class Bot {
       }))
     })
   }
+
   /**
    * 获取当前账号在线客户端列表
    * @param no_cache 是否无视缓存
@@ -165,6 +170,7 @@ export class Bot {
       }))
     })
   }
+
   /**
    * 获取陌生人信息
    * @param user_id QQ 号
@@ -188,6 +194,7 @@ export class Bot {
       }))
     })
   }
+
   /**
    * 获取好友列表
    * 该 API 无需参数
@@ -206,6 +213,7 @@ export class Bot {
       }))
     })
   }
+
   /**
    * 获取单向好友列表
    * 该 API 无需参数
@@ -224,6 +232,7 @@ export class Bot {
       }))
     })
   }
+
   /**
    * 删除好友
    * 该 API 无响应数据
@@ -246,6 +255,7 @@ export class Bot {
       }))
     })
   }
+
   /**
    * 删除单向好友
    * 该 API 无响应数据
@@ -300,6 +310,7 @@ export class Bot {
       );
     });
   }
+
   /**
   * 发送私聊消息
   * @param user_id QQ号
@@ -310,7 +321,7 @@ export class Bot {
     user_id: number,
     msg: string,
     auto_escape: boolean,
-   
+
   ) {
     return new Promise((resolve) => {
       const echo = uuid.v4();
@@ -331,16 +342,17 @@ export class Bot {
         })
       );
     })
-  } 
-/**
- * 发送消息
- * @param message 要发送的内容
- * @param message_type 消息类型, 支持 private、group , 分别对应私聊、群组, 如不传入, 则根据传入的 *_id 参数判断
- * @param user_id 对方 QQ 号 ( 消息类型为 private 时需要 )
- * @param group_id 群号 ( 消息类型为 group 时需要 )
- * @param auto_escape 消息内容是否作为纯文本发送 ( 即不解析 CQ 码 ) , 只在 message 字段是字符串时有效
- */
-  public send_msg(message: string, message_type?: string,user_id?:number,group_id?:number,auto_escape:boolean=false) {
+  }
+
+  /**
+   * 发送消息
+   * @param message 要发送的内容
+   * @param message_type 消息类型, 支持 private、group , 分别对应私聊、群组, 如不传入, 则根据传入的 *_id 参数判断
+   * @param user_id 对方 QQ 号 ( 消息类型为 private 时需要 )
+   * @param group_id 群号 ( 消息类型为 group 时需要 )
+   * @param auto_escape 消息内容是否作为纯文本发送 ( 即不解析 CQ 码 ) , 只在 message 字段是字符串时有效
+   */
+  public send_msg(message: string, message_type?: string, user_id?: number, group_id?: number, auto_escape: boolean = false) {
     return new Promise((resolve) => {
       const echo = uuid.v4();
 
@@ -352,8 +364,8 @@ export class Bot {
         JSON.stringify({
           action: "send_msg",
           params: {
-            message_type:message_type,
-            group_id:group_id,
+            message_type: message_type,
+            group_id: group_id,
             user_id: user_id,
             message: message,
             auto_escape: auto_escape,
@@ -363,11 +375,12 @@ export class Bot {
       );
     })
   }
+
   /**
    * 获取消息
    * @param message_id 消息id
    */
-  public get_msg(message_id:number) {
+  public get_msg(message_id: number) {
     return new Promise((resolve) => {
       const echo = uuid.v4();
 
@@ -386,6 +399,7 @@ export class Bot {
       );
     })
   }
+
   /**
     * 撤回消息
     * 该 API 无响应数据
@@ -410,6 +424,7 @@ export class Bot {
       );
     })
   }
+
   /** 
    * 标记消息已读
    * 该 API 无响应数据
@@ -434,12 +449,13 @@ export class Bot {
       );
     })
   }
+
   /**
    * 获取合并转发内容
    * 字段 message_id 对应合并转发中的 id 字段
    * @param message_id 消息id
    */
-  public get_forward_msg(message_id:number) {
+  public get_forward_msg(message_id: number) {
     return new Promise((resolve) => {
       const echo = uuid.v4();
 
@@ -458,12 +474,13 @@ export class Bot {
       );
     })
   }
+
   /**
    * 发送合并转发 ( 群聊 )
    * @param group_id 群号
    * @param message 自定义转发消息, 具体看 https://docs.go-cqhttp.org/cqcode
    */
-  public send_group_forward_msg(group_id:number,message:MessageType) {
+  public send_group_forward_msg(group_id: number, message: MessageType) {
     return new Promise((resolve) => {
       const echo = uuid.v4();
 
@@ -475,7 +492,7 @@ export class Bot {
         JSON.stringify({
           action: "send_group_forward_msg",
           params: {
-            group_id:group_id,
+            group_id: group_id,
             message: message,
           },
           echo: echo,
@@ -483,6 +500,7 @@ export class Bot {
       );
     })
   }
+
   /**
   * 发送合并转发 ( 好友 )
   * @param user_id 好友QQ号
@@ -508,6 +526,7 @@ export class Bot {
       );
     })
   }
+
   /**
   * 获取群消息历史记录
   * 不提供起始序号将默认获取最新的消息
@@ -532,6 +551,493 @@ export class Bot {
           echo: echo,
         })
       );
+    })
+  }
+
+  /**
+   * 获取图片信息
+   * @param file 图片缓存文件名
+   */
+  public get_image(file: string) {
+    return new Promise((resolve) => {
+      const echo = uuid.v4();
+
+      this.echoEvent.once(echo, (data) => {
+        resolve(data)
+      });
+
+      this.bot.send(
+        JSON.stringify({
+          action: "get_image",
+          params: {
+            file: file
+          },
+          echo: echo,
+        })
+      );
+    })
+  }
+
+  /**
+   * 检查是否可以发送图片
+   * 该 API 无需参数
+   */
+  public can_send_image() {
+    return new Promise((resolve) => {
+      const echo = uuid.v4();
+
+      this.echoEvent.once(echo, (data) => {
+        resolve(data)
+      });
+
+      this.bot.send(
+        JSON.stringify({
+          action: "can_send_image",
+          echo: echo,
+        })
+      );
+    })
+  }
+
+  /**
+   * 图片 OCR
+   * 目前图片OCR接口仅支持接受的图片
+   * ocr_image API移除了实验模式, 目前版本 .ocr_image 和 ocr_image 均能访问, 后期将只保留后者.
+   * @param image 图片id
+   */
+  public ocr_image(image: string) {
+    return new Promise((resolve) => {
+      const echo = uuid.v4();
+
+      this.echoEvent.once(echo, (data) => {
+        resolve(data)
+      });
+
+      this.bot.send(
+        JSON.stringify({
+          action: "ocr_image",
+          params: {
+            image: image
+          },
+          echo: echo,
+        })
+      );
+    })
+  }
+
+  /**
+   * 获取语音
+   * 要使用此接口, 通常需要安装 ffmpeg, 请参考 OneBot 实现的相关说明。
+   * @param file 收到的语音文件名（消息段的 file 参数）, 如 0B38145AA44505000B38145AA4450500.silk
+   * @param out_format 要转换到的格式, 目前支持 mp3、amr、wma、m4a、spx、ogg、wav、flac
+   */
+  public get_record(file: string, out_format: string) {
+    return new Promise((resolve) => {
+      const echo = uuid.v4();
+
+      this.echoEvent.once(echo, (data) => {
+        resolve(data)
+      });
+      this.bot.send(JSON.stringify({
+        action: "get_record",
+        params: {
+          file: file,
+          out_format: out_format
+        },
+        echo: echo
+      }))
+    })
+  }
+
+  /**
+ * 检查是否可以发送语音
+ * 该 API 无需参数
+ */
+  public can_send_record() {
+    return new Promise((resolve) => {
+      const echo = uuid.v4();
+
+      this.echoEvent.once(echo, (data) => {
+        resolve(data)
+      });
+
+      this.bot.send(
+        JSON.stringify({
+          action: "can_send_record",
+          echo: echo,
+        })
+      );
+    })
+  }
+
+  /**
+   * 处理加好友请求
+   * @param flag 加好友请求的 flag（需从上报的数据中获得）
+   * @param approve 是否同意请求 (默认:true)
+   * @param remark 添加后的好友备注（仅在同意时有效,默认空）
+   */
+  public set_friend_add_request(flag: string, approve: boolean = true, remark: string = "") {
+    return new Promise((resolve) => {
+      const echo = uuid.v4()
+
+      this.echoEvent.once(echo, (data) => {
+        resolve(data)
+      })
+
+      this.bot.send(JSON.stringify({
+        action: "set_friend_add_request",
+        params: {
+          flag: flag,
+          approve: approve,
+          remark: remark
+        },
+        echo: echo
+      }))
+    })
+  }
+
+  /**
+   * 处理加群请求／邀请
+   * @param flag 加好友请求的 
+   * @param sub_type 
+   * @param approve 是否同意请求 (默认:true)
+   * @param reason 拒绝理由（仅在拒绝时有效,默认空)
+   */
+  public set_group_add_request(flag: string, sub_type: "add" | "invite", approve: boolean = true, reason: string = "") {
+    return new Promise((resolve) => {
+      const echo = uuid.v4()
+
+      this.echoEvent.once(echo, (data) => {
+        resolve(data)
+      })
+
+      this.bot.send(JSON.stringify({
+        action: "set_group_add_request",
+        params: {
+          flag: flag,
+          sub_type: sub_type,
+          approve: approve,
+          reason: reason
+        },
+        echo: echo
+      }))
+    })
+  }
+
+  /**
+   * 获取群信息
+   * @param group_id 群号
+   * @param no_cache 是否不使用缓存（使用缓存可能更新不及时, 但响应更快,默认:false)
+   */
+  public get_group_info(group_id: number, no_cache: boolean = false) {
+    return new Promise((resolve) => {
+      const echo = uuid.v4()
+
+      this.echoEvent.once(echo, (data) => {
+        resolve(data)
+      })
+
+      this.bot.send(JSON.stringify({
+        action: "get_group_info",
+        params: {
+          group_id: group_id,
+          no_cache: no_cache
+        },
+        echo: echo
+      }))
+    })
+  }
+
+  /**
+   * 获取群列表
+   * @param no_cache 是否不使用缓存（使用缓存可能更新不及时, 但响应更快,默认:false)
+   */
+  public get_group_list(no_cache: boolean = false) {
+    return new Promise((resolve) => {
+      const echo = uuid.v4()
+
+      this.echoEvent.once(echo, (data) => {
+        resolve(data)
+      })
+
+      this.bot.send(JSON.stringify({
+        action: "get_group_list",
+        params: {
+          no_cache: no_cache
+        },
+        echo: echo
+      }))
+    })
+  }
+
+  /**
+   * 获取群成员信息
+   * @param group_id 群号
+   * @param user_id QQ号
+   * @param no_cache 是否不使用缓存（使用缓存可能更新不及时, 但响应更快,默认:false)
+   */
+  public get_group_menber_info(group_id: number, user_id: number, no_cache: boolean = false) {
+    return new Promise((resolve) => {
+      const echo = uuid.v4()
+
+      this.echoEvent.once(echo, (data) => {
+        resolve(data)
+      })
+
+      this.bot.send(JSON.stringify({
+        action: "get_group_menber_info",
+        params: {
+          group_id: group_id,
+          user_id: user_id,
+          no_cache: no_cache
+        },
+        echo: echo
+      }))
+    })
+  }
+
+  /**
+   * 获取群成员列表
+   * @param group_id 群号
+   * @param no_cache 是否不使用缓存（使用缓存可能更新不及时, 但响应更快,默认:false)
+   */
+  public get_group_menber_list(group_id: number, no_cache: boolean = false) {
+    return new Promise((resolve) => {
+      const echo = uuid.v4()
+
+      this.echoEvent.once(echo, (data) => {
+        resolve(data)
+      })
+
+      this.bot.send(JSON.stringify({
+        action: "get_group_menber_list",
+        params: {
+          group_id: group_id,
+          no_cache: no_cache
+        },
+        echo: echo
+      }))
+    })
+  }
+
+  /**
+   * 获取群成员列表
+   * @param group_id 群号
+   * @param type 	要获取的群荣誉类型, 可传入 talkative performer legend strong_newbie emotion 以分别获取单个类型的群荣誉数据, 或传入 all 获取所有数据
+   */
+  public get_group_honor_info(group_id: number, type: "talkactive" | "performer" | "legend" | "strong_newbie" | "emotion" | "all") {
+    return new Promise((resolve) => {
+      const echo = uuid.v4()
+
+      this.echoEvent.once(echo, (data) => {
+        resolve(data)
+      })
+
+      this.bot.send(JSON.stringify({
+        action: "get_group_honor_info",
+        params: {
+          group_id: group_id,
+          type: type
+        },
+        echo: echo
+      }))
+    })
+  }
+
+  /**
+   * 获取群系统消息
+   * 应该是没有参数，毕竟go-cq文档没写
+   */
+  public get_group_system_msg() {
+    return new Promise((resolve) => {
+      const echo = uuid.v4()
+
+      this.echoEvent.once(echo, (data) => {
+        resolve(data)
+      })
+
+      this.bot.send(JSON.stringify({
+        action: "get_group_system_msg",
+        echo: echo
+      }))
+    })
+  }
+
+  /**
+   * 获取精华消息列表
+   * @param group_id 群号
+   */
+  public get_essence_msg_list(group_id: number) {
+    return new Promise((resolve) => {
+      const echo = uuid.v4()
+
+      this.echoEvent.once(echo, (data) => {
+        resolve(data)
+      })
+
+      this.bot.send(JSON.stringify({
+        action: "get_essence_msg_list",
+        params: {
+          group_id: group_id
+        },
+        echo: echo
+      }))
+    })
+  }
+
+  /**
+   * 获取群 \@全体成员 剩余次数
+   * @param group_id 群号
+   */
+  public get_group_at_all_remain(group_id: number) {
+    return new Promise((resolve) => {
+      const echo = uuid.v4()
+
+      this.echoEvent.once(echo, (data) => {
+        resolve(data)
+      })
+
+      this.bot.send(JSON.stringify({
+        action: "get_group_at_all_remain",
+        params: {
+          group_id: group_id
+        },
+        echo: echo
+      }))
+    })
+  }
+
+  /**
+   * 设置群名
+   * 该 API 无响应数据
+   * @param group_id 群号
+   * @param group_name 新群名
+   */
+  public set_group_name(group_id: number, group_name: string) {
+    return new Promise((resolve) => {
+      const echo = uuid.v4()
+
+      this.echoEvent.once(echo, (data) => {
+        resolve(data)
+      })
+
+      this.bot.send(JSON.stringify({
+        action: "set_group_name",
+        params: {
+          group_id: group_id,
+          group_name: group_name
+        },
+        echo: echo
+      }))
+    })
+  }
+
+  /**
+   * 设置群头像
+   * @param group_id 群号
+   * @param file 图片文件名
+   * @param cache 表示是否使用已缓存的文件
+   */
+  public set_group_portrait(group_id: number, file: string, cache: number = 1) {
+    return new Promise((resolve) => {
+      const echo = uuid.v4()
+
+      this.echoEvent.once(echo, (data) => {
+        resolve(data)
+      })
+
+      this.bot.send(JSON.stringify({
+        action: "set_group_portrait",
+        params: {
+          group_id: group_id,
+          file: file,
+          cache: cache
+        },
+        echo: echo
+      }))
+    })
+  }
+
+  /**
+   * 设置群管理员
+   * 该 API 无响应数据
+   * @param group_id 群号
+   * @param user_id 要设置管理员的 QQ 号
+   * @param enable true 为设置, false 为取消
+   */
+  public set_group_admin(group_id: number, user_id: number, enable: boolean = true) {
+    return new Promise((resolve) => {
+      const echo = uuid.v4()
+
+      this.echoEvent.once(echo, (data) => {
+        resolve(data)
+      })
+
+      this.bot.send(JSON.stringify({
+        action: "set_group_admin",
+        params: {
+          group_id: group_id,
+          user_id: user_id,
+          enable: enable
+        },
+        echo: echo
+      }))
+    })
+  }
+
+  /**
+  * 设置群名片 ( 群备注 )
+  * 该 API 无响应数据
+  * @param group_id 群号
+  * @param user_id 要设置的 QQ 号
+  * @param card 群名片内容, 不填或空字符串表示删除群名片
+  */
+  public set_group_card(group_id: number, user_id: number, card: string = "") {
+    return new Promise((resolve) => {
+      const echo = uuid.v4()
+
+      this.echoEvent.once(echo, (data) => {
+        resolve(data)
+      })
+
+      this.bot.send(JSON.stringify({
+        action: "set_group_card",
+        params: {
+          group_id: group_id,
+          user_id: user_id,
+          card: card
+        },
+        echo: echo
+      }))
+    })
+  }
+
+  /**
+    * 设置群组专属头衔
+    * 该 API 无响应数据
+    * @param group_id 群号
+    * @param user_id 要设置的 QQ 号
+    * @param special_title 专属头衔, 不填或空字符串表示删除专属头衔
+    * @param duration 专属头衔有效期, 单位秒, -1 表示永久, 不过此项似乎没有效果, 可能是只有某些特殊的时间长度有效, 有待测试
+    */
+  public set_group_special_title(group_id: number, user_id: number, special_title: string = "", duration: number = -1) {
+    return new Promise((resolve) => {
+      const echo = uuid.v4()
+
+      this.echoEvent.once(echo, (data) => {
+        resolve(data)
+      })
+
+      this.bot.send(JSON.stringify({
+        action: "set_group_special_title",
+        params: {
+          group_id: group_id,
+          user_id: user_id,
+          special_title: special_title,
+          duration: duration
+        },
+        echo: echo
+      }))
     })
   }
 }  
