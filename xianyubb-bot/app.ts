fdfee8df80b6ad5f144029777916f8799092aafa
xianyubb <2149656630@qq.com>
2023-08-17 18:55:02 +0800

    写了个插件出错自重启

--- xianyubb-bot/app.ts
+++ xianyubb-bot/app.ts
@@ -2,7 +2,8 @@ import * as fs from "fs"
 import { Bot } from "./src/api";
 import *as keypress from "keypress"
 import WebSocket = require("ws");
-import path = require("path");
+import { spawn } from 'child_process'
+import * as path from 'path';
 
 const Path = "./config/config.json"
 function mkdir() {
@@ -12,7 +13,7 @@ function mkdir() {
       address: "127.0.0.1",
       port: 8080,
     }))
-    }
+  }
   if (!fs.existsSync("./plugins")) {
     fs.mkdirSync("./plugins")
   }
@@ -72,20 +73,72 @@ function exit() {
   process.stdin.resume();
 }
 
-
+let restartCount = 0;
 
 bot.bot.onopen = () => {
   console.log("连接成功");
-  console.log("正在加载插件");
+  console.log("正在加载插件")
   const pluginsDir = './plugins'; // plugins文件夹路径
-  let pluginarr = []
-  fs.readdirSync(pluginsDir).forEach(file => {
-    const pluginPath = path.join(pluginsDir, file);
-    if (path.extname(pluginPath) === '.js') {
-      import(path.resolve(pluginPath)).then(() => {
-       let a=  pluginarr.push(file)
-        console.log("已加载插件: " +a + "个")
-      })
+  let pluginarr: string[] = [];
+  let restartCount = readRestartCount(); // 从文件中读取重启计数器的值
+  const maxRestartCount = 3; // 最大重启次数
+  
+  async function loadPlugins() {
+    try {
+      const pluginFiles = await fs.promises.readdir(pluginsDir);
+      for (const file of pluginFiles) {
+        if (path.extname(file) === '.js') {
+          try {
+            const pluginModule = await import(`.\\plugins\\${file}`);
+            // 在这里执行插件的初始化和使用操作
+            console.log(`已加载插件: ${file}`);
+            pluginarr.push(file);
+          } catch (error) {
+            console.error(`加载插件时出错: ${file}`);
+            console.error(error);
+            restartProgram(); // 发生错误时重启程序
+          }
+        }
+      }
+    } catch (error) {
+      console.error('读取插件文件夹时出错');
+      console.error(error);
+      restartProgram(); // 发生错误时重启程序
     }
-  })
+  }
+  
+  function readRestartCount() {
+    try {
+      const data = fs.readFileSync('restartCount.txt', 'utf8');
+      return parseInt(data);
+    } catch (error) {
+      return 0; // 如果文件不存在或读取错误，则默认为0
+    }
+  }
+  
+  function writeRestartCount(count: number) {
+    fs.writeFileSync('restartCount.txt', count.toString(), 'utf8');
+  }
+  
+  function restartProgram() {
+    restartCount++;
+    if (restartCount <= maxRestartCount) {
+      console.log(`正在重启程序，重启次数: `);
+      writeRestartCount(restartCount); // 将计数器的值写入文件
+      const nodeCmd = process.argv[0]; // 当前 Node.js 可执行文件路径
+      const scriptPath = process.argv[1]; // 当前脚本文件路径
+      const args = process.argv.slice(2); // 命令行参数（去除前两个默认参数）
+      const child = spawn(nodeCmd, [scriptPath, ...args], { stdio: 'inherit' });
+      child.on('exit', (code) => {
+        process.exit(code); // 子进程退出时，父进程也退出，并返回相同的退出码
+      });
+    } else {
+      console.log(`达到最大重启次数: ，重置重启次数`);
+      writeRestartCount(0); // 重置计数器为零
+      process.exit(1);
+    }
+  }
+  
+  loadPlugins();
+  
 }
