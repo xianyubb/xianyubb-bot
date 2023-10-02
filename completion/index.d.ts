189ee780f4e4fe19cc168a906a5b199c766549b9
xianyubb <2149656630@qq.com>
2023-10-03 01:39:34 +0800

    人麻了，补全写明白了，但是修改了源码数据格式，影响不大

--- completion/index.d.ts
+++ completion/index.d.ts
@@ -1,125 +1,385 @@
-import * as data from "./data";
+interface Data {
+  /** 事件发生的unix时间戳 */
+  time: number;
+  /** 收到事件的机器人的 QQ 号 */
+  self_id: number;
+  /** 表示该上报的类型, 消息, 消息发送, 请求, 通知, 或元事件 */
+  post_type: string
+}
+
+interface Anonymous {
+  id: number;
+  name: string;
+  flag: string;
+}
+
+// message消息
+interface MessageType {
+  type: string;
+  data: {
+    text?: string;
+    id?: number | string;
+    file?: string;
+    at?: number;
+    url?: string;
+    title?: string;
+    content?: string;
+    image?: string;
+    type?: "qq" | "group";
+    lat?: string | number;
+    lon?: string | number;
+    audio?: string;
+    qq?: number;
+    time?: number;
+    uin?: number;
+    seq?: MessageType;
+  };
+}
+interface Sender {
+  user_id: number;
+  nickname: string;
+  sex?: string;
+  age?: number;
+  group_id?: number;
+  card?: string;
+  area?: string;
+  level?: string;
+  role?: string;
+  title?: string;
+}
+
+interface Message extends Data {
+  group_id: number;
+  message_type: "private" | "group";
+  sub_type: string;
+  message_id: number;
+  user_id: number;
+  message: string | MessageType[];
+  raw_message: string;
+  font: number;
+  sender: Sender;
+  target_id: number;
+  anonymous: null | Anonymous;
+}
+
+interface GroupMessage extends Data {
+  sub_type: string;
+  message_id: number;
+  user_id: number;
+  message: string | MessageType[];
+  raw_message: string;
+  font: number;
+  sender: Sender;
+  target_id: number;
+  /** 群号 */
+  group_id: number;
+  anonymous: null | Anonymous;
+}
+
+interface PrivateMessage {
+  sub_type: string;
+  message_id: number;
+  user_id: number;
+  message: string | MessageType[];
+  raw_message: string;
+  font: number;
+  sender: Sender;
+  target_id: number;
+}
+
+// notice消息
+interface Notice extends Data {
+  notice_type: string;
+  user_id: number;
+  group_id: number;
+  sub_type: string;
+  operator_id: number;
+  message_id: number;
+  file: file;
+  duration: number;
+  sender_id: number;
+  target_id: number;
+  honor_type: "talkative" | "performer" | "emotion";
+  title: string;
+  card_new: string;
+  card_old: string;
+  client: any;
+  online: boolean;
+}
+
+interface file {
+  id: string,
+  name: string,
+  size: number,
+  url: string,
+  busi: number
+}
+
+interface PrivateDeleteMsg {
+  user_id: number;
+  message_id: number;
+}
+
+interface GroupDeleteMsg {
+  group_id: number;
+  operator_id: number;
+  user_id: number;
+  message_id: number;
+}
+
+interface GroupMemberAdd {
+  sub_type: string;
+  user_id: number;
+  group_id: number;
+  operator_id: number;
+}
+
+interface GroupMemberDecrease {
+  sub_type: string;
+  group_id: number;
+  operator_id: number;
+  user_id: number;
+}
+
+interface GroupAdminChange {
+  sub_type: string;
+  group_id: number;
+  user_id: number;
+}
+
+interface GroupfileUpload {
+  group_id: number;
+  user_id: number;
+  file: file;
+}
+interface GroupBan {
+  sub_type: string;
+  group_id: number;
+  operator_id: number;
+  user_id: number;
+  duration: number;
+}
+
+interface FrienAdd {
+  user_id: number;
+}
+
+interface Notify {
+  sender_id: number;
+  user_id: number;
+  target_id: number;
+  group_id: number;
+}
+
+interface GroupRedbagLuckyKing {
+  group_id: number;
+  user_id: number;
+  target_id: number;
+}
+
+interface GroupMemberHonorChange {
+  group_id: number;
+  user_id: number;
+  honor_type: string;
+}
+
+interface GroupMemberTitleChange {
+  group_id: number;
+  user_id: number;
+  title: string;
+}
+
+interface GroupCardChange {
+  group_id: number;
+  user_id: number;
+  card_new: string;
+  card_old: string;
+}
+
+interface ReceiveOfflinefile {
+  user_id: number;
+  file: file;
+}
+
+interface ClientStatusChange {
+  client: any;
+  online: boolean;
+}
+
+interface EssenceMessageChange {
+  sub_type: string;
+  group_id: number;
+  sender_id: number;
+  operator_id: number;
+  message_id: number;
+}
+
+// request消息
+interface Request extends Data {
+  request_type: string;
+  user_id: number;
+  comment: string;
+  flag: string;
+  sub_type: string;
+  group_id: number;
+}
+
+interface AddFriendRequest {
+  user_id: number;
+  comment: string;
+  flag: string;
+}
+
+interface AddGroupRequest {
+  user_id: number;
+  group_id: number;
+  comment: string;
+  sub_type: string;
+  flag: string;
+}
+
+// meta_event
+interface MetaEvent extends Data {
+  meta_event_type: string;
+  status: Status;
+  interval: number;
+  sub_type: string;
+}
+
+interface Status {
+  app_initialized: boolean;
+  app_enabled: boolean;
+  plugins_good: boolean;
+  app_good: boolean;
+  online: boolean;
+  stat: boolean;
+}
+
+interface HeartBeat {
+  status: Status;
+  interval: number;
+}
+
+interface LifeCycle {
+  sub_type: string;
+}
+
+// echo
+interface Returnecho {
+  status: string;
+  retcode: number;
+  data: any;
+  uuid: string;
+}
 
 declare namespace bot {
   namespace BotEvents {
     /** 收到群聊消息 */
     function on(
       event: "onReceiveGroupMessage",
-      listener: (msg: data.GroupMessage) => void
+      listener: (msg: GroupMessage) => void
     ): any;
     /** 收到私聊消息 */
     function on(
       event: "onReceivePrivateMessage",
-      listener: (msg: data.PrivateMessage) => void
+      listener: (msg: PrivateMessage) => void
     ): any;
     /** 私聊消息撤回 */
     function on(
       Event: "onPrivateDeleteMsg",
-      listener: (msg: data.PrivateDeleteMsg) => void
+      listener: (msg: PrivateDeleteMsg) => void
     ): any;
     /** 群聊消息撤回 */
     function on(
       Event: "onGroupDeteleMsg",
-      listener: (msg: data.GroupDeleteMsg) => void
+      listener: (msg: GroupDeleteMsg) => void
     ): any;
     /** 群聊成员增加 */
     function on(
       Event: "onGroupMemberAdd",
-      listener: (msg: data.GroupMemberAdd) => void
+      listener: (msg: GroupMemberAdd) => void
     ): any;
     /** 群聊成员减少 */
     function on(
       Event: "onGroupMemberDecrease",
-      listener: (msg: data.GroupMemberDecrease) => void
+      listener: (msg: GroupMemberDecrease) => void
     ): any;
     /** 群聊管理员变动 */
     function on(
       Event: "onGroupAdminChange",
-      listener: (msg: data.GroupAdminChange) => void
+      listener: (msg: GroupAdminChange) => void
     ): any;
     /** 群聊文件上传 */
     function on(
-      Event: "onGroupFileUpload",
-      listener: (msg: data.GroupFileUpload) => void
+      Event: "onGroupfileUpload",
+      listener: (msg: GroupfileUpload) => void
     ): any;
     /** 群聊禁言 */
-    function on(
-      Event: "onGroupBan",
-      listener: (msg: data.GroupBan) => void
-    ): any;
+    function on(Event: "onGroupBan", listener: (msg: GroupBan) => void): any;
     /** 好友添加 */
-    function on(
-      Event: "onFriendAdd",
-      listener: (msg: data.FrienAdd) => void
-    ): any;
+    function on(Event: "onFriendAdd", listener: (msg: FrienAdd) => void): any;
     /** 戳一戳
      * 此事件无法在手表协议上触发
      */
-    function on(Event: "onNotify", listener: (msg: data.Notify) => void): any;
+    function on(Event: "onNotify", listener: (msg: Notify) => void): any;
     /** 群聊红包运气王提示
      * 此事件无法在手表协议上触发
      */
     function on(
       Event: "onGroupRedbagLuckyKing",
-      listener: (msg: data.GroupRedbagLuckyKing) => void
+      listener: (msg: GroupRedbagLuckyKing) => void
     ): any;
     /** 群聊成员荣誉变更提示
      * 此事件无法在手表协议上触发
      */
     function on(
       Event: "onGroupMemberHonorChange",
-      listener: (msg: data.GroupMemberHonorChange) => void
+      listener: (msg: GroupMemberHonorChange) => void
     ): any;
     /** 群聊成员头衔变更 */
     function on(
       Event: "onGroupMemberTitleChange",
-      listener: (msg: data.GroupMemberTitleChange) => void
+      listener: (msg: GroupMemberTitleChange) => void
     ): any;
     /** 群聊成员名片更新
      * 此事件不保证时效性, 仅在收到消息时校验卡片
      */
     function on(
       Event: "onGroupCardChange",
-      listener: (msg: data.GroupCardChange) => void
+      listener: (msg: GroupCardChange) => void
     ): any;
     /** 接收到离线文件 */
     function on(
-      Event: "onReceiveOfflineFile",
-      listener: (msg: data.ReceiveOfflineFile) => void
+      Event: "onReceiveOfflinefile",
+      listener: (msg: ReceiveOfflinefile) => void
     ): any;
     /** 其他客户端在线状态变更 */
     function on(
       Event: "onClientStatusChange",
-      listener: (msg: data.ClientStatusChange) => void
+      listener: (msg: ClientStatusChange) => void
     ): any;
     /** 精华消息变更 */
     function on(
       Event: "onEssenceMessageChange",
-      listener: (msg: data.EssenceMessageChange) => void
+      listener: (msg: EssenceMessageChange) => void
     ): any;
     /** 加好友请求 */
     function on(
       Event: "onAddFriendRequest",
-      listener: (msg: data.AddFriendRequest) => void
+      listener: (msg: AddFriendRequest) => void
     ): any;
     /** 加群聊/邀请 */
     function on(
       Event: "onAddGroupRequest",
-      listener: (msg: data.AddGroupRequest) => void
+      listener: (msg: AddGroupRequest) => void
     ): any;
     /** 心跳包 */
-    function on(
-      Event: "onHeartBeat",
-      listener: (msg: data.HeartBeat) => void
-    ): any;
+    function on(Event: "onHeartBeat", listener: (msg: HeartBeat) => void): any;
     /** 生命周期 */
-    function on(
-      Event: "onLifeCycle",
-      listener: (msg: data.LifeCycle) => void
-    ): any;
+    function on(Event: "onLifeCycle", listener: (msg: LifeCycle) => void): any;
   }
   /** 获取登录号信息 */
-  function get_login_info(): Promise<any>;
+  function get_login_info(): Promise<Returnecho>
   /**
    * 设置登录号资料 (该api没有响应数据)
    * @param nickname 名称
@@ -134,6 +394,5 @@ declare namespace bot {
     email: string,
     college: string,
     personal_note: string
-  ):Promise<any>
-
+  ): Promise<Returnecho>
 }
--- xianyubb-bot/src/data.ts
+++ xianyubb-bot/src/data.ts
@@ -1,6 +1,9 @@
 export interface Data {
-  time: number,
-  self_id: number,
+  /** 事件发生的unix时间戳 */
+  time: number;
+  /** 收到事件的机器人的 QQ 号 */
+  self_id: number;
+  /** 表示该上报的类型, 消息, 消息发送, 请求, 通知, 或元事件 */
   post_type: string
 }
 
@@ -45,7 +48,6 @@ export interface Sender {
   title?: string
 }
 
-
 export interface Message extends Data {
   group_id: number
   message_type: "private" | "group"
@@ -57,31 +59,50 @@ export interface Message extends Data {
   font: number,
   sender: Sender,
   target_id: number,
-  anonymous: (null | Anonymous)
+  anonymous: (null | Anonymous),
+  temp_source: number
 }
 
-export interface GroupMessage {
+export interface GroupMessage extends Data {
+  /** 消息子类型, 正常消息是 normal, 匿名消息是 anonymous, 系统提示 ( 如「管理员已禁止群内匿名聊天」 ) 是 notice */
   sub_type: string,
+  /** 消息 ID */
   message_id: number,
+  /** 发送者 QQ 号 */
   user_id: number,
+  /** 消息内容 */
   message: string | MessageType[],
+  /** 原始消息内容 */
   raw_message: string,
+  /** 字体 */
   font: number,
+  /** 发送者信息 */
   sender: Sender,
-  target_id: number,
+  /** 群号 */
   group_id: number,
+  /** 匿名信息, 如果不是匿名消息则为 null */
   anonymous: (null | Anonymous)
 }
 
-export interface PrivateMessage {
+export interface PrivateMessage extends Data {
+  /** 消息子类型, 如果是好友则是 friend, 如果是群临时会话则是 group, 如果是在群中自身发送则是 group_self */
   sub_type: string,
+  /** 消息 ID */
   message_id: number,
+  /** 发送者 QQ 号 */
   user_id: number,
+  /** 消息内容 */
   message: string | MessageType[],
+  /** 原始消息内容  */
   raw_message: string,
+  /** 字体 */
   font: number,
+  /** 发送者信息 */
   sender: Sender,
+  /** 接受者 QQ 号 */
   target_id: number,
+  /** 临时会话来源 */
+  temp_source: number
 }
 
 const message = (data: Message, callback: (Event: string, data: any) => void) => {
@@ -97,7 +118,9 @@ const message = (data: Message, callback: (Event: string, data: any) => void) =>
         raw_message: data.raw_message,
         sender: data.sender,
         anonymous: data.anonymous,
-        target_id: data.target_id,
+        time: data.time,
+        self_id: data.self_id,
+        post_type: data.post_type
       }
       callback("onReceiveGroupMessage", GroupMessage)
       break;
@@ -110,7 +133,11 @@ const message = (data: Message, callback: (Event: string, data: any) => void) =>
         raw_message: data.raw_message,
         font: data.font,
         sender: data.sender,
-        target_id: data.target_id
+        target_id: data.target_id,
+        temp_source: data.temp_source,
+        time: data.time,
+        self_id: data.self_id,
+        post_type: data.post_type
       }
       callback("onReceivePrivateMessage", PrivateMessage)
       break;
@@ -125,7 +152,7 @@ export interface Notice extends Data {
   sub_type: string,
   operator_id: number,
   message_id: number,
-  file: File,
+  file: file,
   duration: number,
   sender_id: number,
   target_id: number,
@@ -137,7 +164,7 @@ export interface Notice extends Data {
   online: boolean
 }
 
-export interface File {
+export interface file {
   id: string,
   name: string,
   size: number,
@@ -145,44 +172,44 @@ export interface File {
   busi: number
 }
 
-export interface PrivateDeleteMsg {
+export interface PrivateDeleteMsg extends Data {
   user_id: number
   message_id: number
 }
 
-export interface GroupDeleteMsg {
+export interface GroupDeleteMsg extends Data {
   group_id: number,
   operator_id: number
   user_id: number
   message_id: number
 }
 
-export interface GroupMemberAdd {
+export interface GroupMemberAdd extends Data {
   sub_type: string
   user_id: number
   group_id: number,
   operator_id: number,
 }
 
-export interface GroupMemberDecrease {
+export interface GroupMemberDecrease extends Data {
   sub_type: string
   group_id: number,
   operator_id: number,
   user_id: number
 }
 
-export interface GroupAdminChange {
+export interface GroupAdminChange extends Data {
   sub_type: string,
   group_id: number,
   user_id: number
 }
 
-export interface GroupFileUpload {
+export interface GroupFileUpload extends Data {
   group_id: number,
   user_id: number,
-  file: File
+  file: file
 }
-export interface GroupBan {
+export interface GroupBan extends Data {
   sub_type: string,
   group_id: number,
   operator_id: number,
@@ -190,53 +217,53 @@ export interface GroupBan {
   duration: number
 }
 
-export interface FrienAdd {
+export interface FrienAdd extends Data {
   user_id: number
 }
 
-export interface Notify {
+export interface Notify extends Data {
   sender_id: number,
   user_id: number,
   target_id: number
   group_id: number
 }
 
-export interface GroupRedbagLuckyKing {
+export interface GroupRedbagLuckyKing extends Data {
   group_id: number,
   user_id: number,
   target_id: number
 }
 
-export interface GroupMemberHonorChange {
+export interface GroupMemberHonorChange extends Data {
   group_id: number,
   user_id: number,
   honor_type: string
 }
 
-export interface GroupMemberTitleChange {
+export interface GroupMemberTitleChange extends Data {
   group_id: number,
   user_id: number,
   title: string
 }
 
-export interface GroupCardChange {
+export interface GroupCardChange extends Data {
   group_id: number,
   user_id: number,
   card_new: string,
   card_old: string,
 }
 
-export interface ReceiveOfflineFile {
+export interface ReceiveOfflineFile extends Data {
   user_id: number,
-  file: File
+  file: file
 }
 
-export interface ClientStatusChange {
+export interface ClientStatusChange extends Data {
   client: any,
   online: boolean
 }
 
-export interface EssenceMessageChange {
+export interface EssenceMessageChange extends Data {
   sub_type: string,
   group_id: number,
   sender_id: number,
@@ -250,7 +277,10 @@ export const notice = (data: Notice, callback: (Events: string, msg: any) => voi
     case "friend_recall":
       const PrivateDeleteMsg: PrivateDeleteMsg = {
         user_id: data.user_id,
-        message_id: data.message_id
+        message_id: data.message_id,
+        time: data.time,
+        self_id: data.self_id,
+        post_type: data.post_type
       }
       callback("onPrivateDeleteMsg", PrivateDeleteMsg)
       break;
@@ -260,6 +290,9 @@ export const notice = (data: Notice, callback: (Events: string, msg: any) => voi
         operator_id: data.operator_id,
         user_id: data.user_id,
         message_id: data.message_id,
+        time: data.time,
+        self_id: data.self_id,
+        post_type: data.post_type
       }
       callback("onGroupDeleteMsg", GroupDeleteMsg)
       break
@@ -268,7 +301,10 @@ export const notice = (data: Notice, callback: (Events: string, msg: any) => voi
         sub_type: data.sub_type,
         group_id: data.group_id,
         user_id: data.user_id,
-        operator_id: data.operator_id
+        operator_id: data.operator_id,
+        time: data.time,
+        self_id: data.self_id,
+        post_type: data.post_type
       }
       callback("onGroupMemberAdd", GroupMemberAdd)
       break
@@ -277,7 +313,10 @@ export const notice = (data: Notice, callback: (Events: string, msg: any) => voi
         sub_type: data.sub_type,
         group_id: data.group_id,
         operator_id: data.operator_id,
-        user_id: data.user_id
+        user_id: data.user_id,
+        time: data.time,
+        self_id: data.self_id,
+        post_type: data.post_type
       }
       callback("onGroupMemberDecrease", GroupMemberDecrease)
       break;
@@ -285,7 +324,10 @@ export const notice = (data: Notice, callback: (Events: string, msg: any) => voi
       const GroupAdminChange: GroupAdminChange = {
         user_id: data.user_id,
         group_id: data.group_id,
-        sub_type: data.sub_type
+        sub_type: data.sub_type,
+        time: data.time,
+        self_id: data.self_id,
+        post_type: data.post_type
       }
       callback("onGroupAdminChange", GroupAdminChange)
       break;
@@ -293,7 +335,10 @@ export const notice = (data: Notice, callback: (Events: string, msg: any) => voi
       const GroupFileUpload: GroupFileUpload = {
         group_id: data.group_id,
         file: data.file,
-        user_id: data.user_id
+        user_id: data.user_id,
+        time: data.time,
+        self_id: data.self_id,
+        post_type: data.post_type
       }
       callback("onGroupFileUpload", GroupFileUpload)
       break;
@@ -303,13 +348,19 @@ export const notice = (data: Notice, callback: (Events: string, msg: any) => voi
         group_id: data.group_id,
         operator_id: data.operator_id,
         sub_type: data.sub_type,
-        duration: data.duration
+        duration: data.duration,
+        time: data.time,
+        self_id: data.self_id,
+        post_type: data.post_type
       }
       callback("onGroupBan", GroupBan)
       break;
     case "friend_add":
       const FriendAdd: FrienAdd = {
         user_id: data.user_id,
+        time: data.time,
+        self_id: data.self_id,
+        post_type: data.post_type
       }
       callback("onFriendAdd", FriendAdd)
       break
@@ -320,7 +371,10 @@ export const notice = (data: Notice, callback: (Events: string, msg: any) => voi
             group_id: data.group_id,
             target_id: data.target_id,
             user_id: data.user_id,
-            sender_id: data.sender_id
+            sender_id: data.sender_id,
+            time: data.time,
+            self_id: data.self_id,
+            post_type: data.post_type
           }
           callback("onNotify", Notify)
           break;
@@ -328,7 +382,10 @@ export const notice = (data: Notice, callback: (Events: string, msg: any) => voi
           const GroupRedbagLuckyKing: GroupRedbagLuckyKing = {
             group_id: data.group_id,
             user_id: data.user_id,
-            target_id: data.target_id
+            target_id: data.target_id,
+            time: data.time,
+            self_id: data.self_id,
+            post_type: data.post_type
           }
           callback("onGroupRedbagLuckyKing", GroupRedbagLuckyKing)
           break
@@ -336,7 +393,10 @@ export const notice = (data: Notice, callback: (Events: string, msg: any) => voi
           const GroupMemberHonorChange: GroupMemberHonorChange = {
             group_id: data.group_id,
             user_id: data.user_id,
-            honor_type: data.honor_type
+            honor_type: data.honor_type,
+            time: data.time,
+            self_id: data.self_id,
+            post_type: data.post_type
           }
           callback("onGroupMemberHonorChange", GroupMemberHonorChange)
           break;
@@ -344,7 +404,10 @@ export const notice = (data: Notice, callback: (Events: string, msg: any) => voi
           const GroupMemberTitleChange: GroupMemberTitleChange = {
             group_id: data.group_id,
             user_id: data.user_id,
-            title: data.title
+            title: data.title,
+            time: data.time,
+            self_id: data.self_id,
+            post_type: data.post_type
           }
           callback("onGroupMemberTitleChange", GroupMemberTitleChange)
           break
@@ -355,14 +418,20 @@ export const notice = (data: Notice, callback: (Events: string, msg: any) => voi
         group_id: data.group_id,
         user_id: data.user_id,
         card_new: data.card_new,
-        card_old: data.card_old
+        card_old: data.card_old,
+        time: data.time,
+        self_id: data.self_id,
+        post_type: data.post_type
       }
       callback("onGroupCardChange", GroupCardChange)
       break;
     case "offline_file":
       const ReceiveOfflineFile: ReceiveOfflineFile = {
         user_id: data.user_id,
-        file: data.file
+        file: data.file,
+        time: data.time,
+        self_id: data.self_id,
+        post_type: data.post_type
       }
       callback("onReceiveOfflineFile", ReceiveOfflineFile)
       break;
@@ -370,6 +439,9 @@ export const notice = (data: Notice, callback: (Events: string, msg: any) => voi
       const ClientStatusChange: ClientStatusChange = {
         client: data.client,
         online: data.online,
+        time: data.time,
+        self_id: data.self_id,
+        post_type: data.post_type
       }
       callback("onClientStatusChange", ClientStatusChange)
       break;
@@ -380,6 +452,9 @@ export const notice = (data: Notice, callback: (Events: string, msg: any) => voi
         sub_type: data.sub_type,
         operator_id: data.operator_id,
         message_id: data.message_id,
+        time: data.time,
+        self_id: data.self_id,
+        post_type: data.post_type
       }
       callback("onEssenceMessageChange", EssenceMessageChange)
       break
@@ -395,13 +470,13 @@ export interface Request extends Data {
   group_id: number,
 }
 
-export interface AddFriendRequest {
+export interface AddFriendRequest extends Data {
   user_id: number,
   comment: string,
   flag: string
 }
 
-export interface AddGroupRequest {
+export interface AddGroupRequest extends Data {
   user_id: number,
   group_id: number,
   comment: string,
@@ -415,7 +490,10 @@ const request = (data: Request, callback: (Events: string, msg: any) => void) =>
       const AddFriendRequest: AddFriendRequest = {
         user_id: data.user_id,
         comment: data.comment,
-        flag: data.flag
+        flag: data.flag,
+        time: data.time,
+        self_id: data.self_id,
+        post_type: data.post_type
       }
       callback("onAddFriendRequest", AddFriendRequest)
       break;
@@ -425,7 +503,10 @@ const request = (data: Request, callback: (Events: string, msg: any) => void) =>
         user_id: data.user_id,
         comment: data.comment,
         flag: data.flag,
-        sub_type: data.sub_type
+        sub_type: data.sub_type,
+        time: data.time,
+        self_id: data.self_id,
+        post_type: data.post_type
       }
       callback("onAddGroupRequest", AddGroupRequest)
       break
@@ -483,6 +564,7 @@ export interface Returnecho {
   data: any,
   uuid: string
 }
+
 export default (msg: any, callback: (Events: string, data: any) => void) => {
   if (msg.post_type) {
     switch (msg.post_type) {
