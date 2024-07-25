ee3e903f61239cb391b55f02e9fba079e45c77a6
xianyubb <2149656630@qq.com>
2024-07-25 13:06:14 +0800

    0.7.0

--- app.ts
+++ app.ts
@@ -16,7 +16,7 @@ logger.log(entryDir);
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const Project_Name = "xianyubb-bot";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const Version = [0, 6, 0];
+const Version = [0, 7, 0];
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const Author = "xianyubb";
 
